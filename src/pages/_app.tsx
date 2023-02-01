155c188b84be495811e47553e9c2a2d4cf520c6a
figurae <aleksander.gielnicki@gmail.com>
2023-02-01 22:37:04 +0100

    Add .env.development

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { NhostProvider, NhostClient } from '@nhost/nextjs';
 
 const nhost = new NhostClient({
 	subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || '',
-	region: process.env.NEXT_PUBLIC_NHOST_REGION || '',
+	//region: process.env.NEXT_PUBLIC_NHOST_REGION || '',
 });
 
 export default function App({ Component, pageProps }: AppProps) {
