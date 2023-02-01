f73fd5130eeba6a8bfd36bd30bdac4be4d7825bb
figurae <aleksander.gielnicki@gmail.com>
2023-02-01 22:27:54 +0100

    Add NhostProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { NhostProvider, NhostClient } from '@nhost/nextjs';
+
+const nhost = new NhostClient({
+	subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || '',
+	region: process.env.NEXT_PUBLIC_NHOST_REGION || '',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<NhostProvider nhost={nhost} initial={pageProps.nhostSession}>
+			<Component {...pageProps} />
+		</NhostProvider>
+	);
 }
