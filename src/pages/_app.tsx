c6269b3cfe72b7af19636f0eb756f3bd002a507d
figurae <aleksander.gielnicki@gmail.com>
2023-02-07 20:20:30 +0100

    Add local nhost support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { NhostProvider, NhostClient } from '@nhost/nextjs';
+import { NhostApolloProvider } from '@nhost/react-apollo';
 
 const nhost = new NhostClient({
 	subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || '',
@@ -10,7 +11,9 @@ const nhost = new NhostClient({
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<NhostProvider nhost={nhost} initial={pageProps.nhostSession}>
-			<Component {...pageProps} />
+			<NhostApolloProvider nhost={nhost}>
+				<Component {...pageProps} />
+			</NhostApolloProvider>
 		</NhostProvider>
 	);
 }
